4542590289a11f43542fb188e32a58ec2f5ddd26
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-05 00:14:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
-
+     "Fizz"
+    
   elsif num % 5 == 0
-    puts "Buzz"
+     "Buzz"
 
   elsif num % 15 == 0
-    puts "FizzBuzz"
+     "FizzBuzz"
 
   else
      nil
